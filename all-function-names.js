1ae28daf3831b05f7cbc349831015a5847444c17
Xiao Liang <yxliang01@users.noreply.github.com>
2019-06-02 01:05:15 +0800

    all-function-names.js: initial complete version

---
+++ all-function-names.js
@@ -0,0 +1,36 @@
+#!node
+
+/**
+  @Author: Xiao Liang <https://github.com/yxliang01>
+  
+  Print all the functions defined in targeted contractName as a JSON array
+  
+  USAGE: ./all-function-names.js TARGET_CONTRACT_NAME
+  
+  Note: before use, install package "solidity-parser-antlr" via npm
+*/
+
+const parser = require("solidity-parser-antlr");
+const fs = require('fs');
+
+const contractName = process.argv[3];
+
+const rst = [];
+
+const ast = parser.parse(fs.readFileSync(process.argv[2], 'utf-8'));
+parser.visit(ast, {
+    ContractDefinition(node) {
+        if(node.name === contractName) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    
+    FunctionDefinition(node) {
+        rst.push(node.name);
+        return false;
+    }
+});
+
+console.log(JSON.stringify(rst));
